d459e97a20c8ed40d650423ae72ac64dfbbb7b0a
SleepingCui <130898297+SleepingCui@users.noreply.github.com>
2023-11-04 19:51:25 +0800

    Update cab.cpp

--- cab.cpp
+++ cab.cpp
@@ -1,36 +1,35 @@
-
 #include <iostream>
 #include <thread>
 #include <vector>
-// 复杂的算法函数
+// Complex algorithm function
 void complexAlgorithm() {
-    // 执行一系列复杂的计算操作
+    // Perform a series of complex calculations
     for (int i = 0; i < 100000000; i++) {
         double result = i * i;
     }
 }
 
 int main() {
-    std::cout << "Welcome use cpu dumper v1.0" << std::endl ;
+    std::cout << "Welcome to the Complex Algorithms Benchmark v1.0" << std::endl ;
     while (1){
-        // 设置线程数
+        // Set the number of threads
         int numThreads;
-        std::cout << "Number of Thread:";
+        std::cout << "Number of Threads:";
         std::cin >> numThreads;
-        // 创建多个线程并运行复杂算法
-        std::cout << "[INFO]starting..." << std::endl;
+        // Create multiple threads and run the complex algorithm
+        std::cout << "[INFO] Starting..." << std::endl;
         std::vector<std::thread> threads;
         for (int i = 0; i < numThreads; i++) {
             threads.push_back(std::thread(complexAlgorithm));
         }
 
-        // 等待所有线程完成
+        // Wait for all threads to complete
         for (auto& thread : threads) {
             thread.join();
         }
 
-        // 输出完成信息
-        std::cout << "[OK]Successful program completion!" << std::endl;
+        // Output completion message
+        std::cout << "[OK] Successful program completion!" << std::endl;
 
     }
 
